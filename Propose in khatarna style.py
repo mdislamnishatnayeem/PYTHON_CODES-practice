9cfe4a134666e041ff0c27183f63d2a39415d057
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2021-03-16 23:03:45 +0600

    Create   Propose in khatarna style.py

---
+++ Propose in khatarna style.py
@@ -0,0 +1,10 @@
+space=""
+for i in range(5):
+    for i in range(40):
+       space=space+" "
+       print(space+"Nice presentation")
+    for l in range(4):
+        print(space+"Nice presentation")
+    for i in range(40):
+       space=space[:-1]
+       print(space+"Nice presentation")
