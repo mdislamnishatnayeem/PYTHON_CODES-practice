4d059b7f143cd83c71f0638698bea5fec285efd1
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2021-03-16 09:58:24 +0000

    Create Identify operators practice.py
    One more added

--- Identify operators practice.py
+++ Identify operators practice.py
@@ -4,3 +4,16 @@ c="Rayeem"
 print(a is b) #output true
 print(a is c) #output false
 print(c is not b) #output true
+
+#One another code.Look bellow.It is a easy code
+
+a=78
+b=89
+c=78
+d=87
+e=57
+f=87
+if(a is b):
+	print("A and B got same marks")
+if(d is f):
+	print("D and f got same marks")
