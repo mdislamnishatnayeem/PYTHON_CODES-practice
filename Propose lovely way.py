1d1bf84de97b2bc4749d85f94cd9cdb302a3972c
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2021-03-16 23:29:22 +0600

    Create Propose lovely way.py

---
+++ Propose lovely way.py
@@ -0,0 +1,13 @@
+space=""
+for i in range(5):
+    for i in range(30):
+       space=space+" "
+       print(space+"A lot of Love to 'Iqra' "+"\U00002764")
+    for l in range(4):
+        print(space+"  	"+"Masa Allah ")
+    for i in range(30):
+       space=space[:-1]
+       print(space+"\U00002764"+"A lot of Love to 'Iqra' ")
+    for l in range(4):
+        print(space+"	"+"Masa Allah")
+    
